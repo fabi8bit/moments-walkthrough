6ab38c6e398ed404507bf8aa6f7c551e9fa0f907
fabi8bit <fabi8bit@gmail.com>
2023-08-29 12:22:47 +0000

    Add sign in form

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Container, Navbar, Nav } from 'react-bootstrap';
 import logo from "../assets/logo.png"
 import styles from "../styles/NavBar.module.css"
 import { NavLink } from "react-router-dom";
+import { CurrentUserContext } from '../App';
 
 
 const NavBar = () => {
+  const currentUser = useContext(CurrentUserContext)
+  const loggedInIcons = <>{currentUser?.username}</>
+  const loggedOutIcons =  
+  <>
+    <NavLink
+    className={styles.NavLink}
+    activeClassName={styles.Active}
+    to="/signin">
+    <i className="fas fa-sign-in-alt"></i>Sign In
+  </NavLink>
+  <NavLink
+    className={styles.NavLink}
+    activeClassName={styles.Active}
+    to="/signup"
+  >
+    <i className="fas fa-user-plus"></i>Sign Up
+  </NavLink>
+</>
+
   return (
     <Navbar className={styles.NavBar} expand="md" fixed="top">
         <Container>
@@ -24,20 +44,9 @@ const NavBar = () => {
                   >
                     <i className="fas fa-home"></i>Home
                   </NavLink>
-                  <NavLink
-                    className={styles.NavLink}
-                    activeClassName={styles.Active}
-                    to="/signin">
-                    <i className="fas fa-sign-in-alt"></i>Sign In
-                  </NavLink>
-                  <NavLink
-                    className={styles.NavLink}
-                    activeClassName={styles.Active}
-                    to="/signup"
-                  >
-                    <i className="fas fa-user-plus"></i>Sign Up
-                  </NavLink>
+                  {currentUser ? loggedInIcons : loggedOutIcons} 
                 </Nav>
+
             </Navbar.Collapse>
         </Container>
     </Navbar>
