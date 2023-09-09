30b46104c8da5130346ca4c0d55993d85bcd53dd
fabi8bit <fabi8bit@gmail.com>
2023-09-09 17:13:55 +0000

    Tidy up and prepare for deployment

--- src/pages/posts/PostEditForm.js
+++ src/pages/posts/PostEditForm.js
@@ -40,7 +40,7 @@ function PostEditForm() {
 
             is_owner ? setPostData({title, content, image}) : history.push('/');
         } catch(err) {
-            console.log(err)
+            // console.log(err)
         }
     };
     handleMount();
@@ -79,7 +79,7 @@ function PostEditForm() {
       await axiosReq.put(`/posts/${id}/`, formData);
       history.push(`/posts/${id}`);
     } catch (err) {
-      console.log(err);
+      // console.log(err);
       if (err.response?.status !== 401) {
         setErrors(err.response?.data);
       }
